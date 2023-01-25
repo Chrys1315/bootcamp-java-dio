d444fae09125afde87a39f7382be532c930c2aa8
My NameLucas <Lucas.chrys269@gmail.com>
2023-01-25 19:53:21 -0300

    Testando o commit dnv

---
+++ projeto-02/src/BoletimEstudantil.java
@@ -0,0 +1,17 @@
+public class BoletimEstudantil {
+   
+    public static void main(String[] args) {
+        int  mediafinal =8;
+        
+        if(mediafinal < 6 ) 
+        System.out.println("Reprovado");
+        else if (mediafinal  == 6)
+        System.out.println("PROVA DE REC");
+        else 
+        System.out.println("Aprovado");
+
+
+        
+    }
+    
+}
